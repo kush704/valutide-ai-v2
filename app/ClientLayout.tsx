a90c1e7cc242d4fea19926388a47181d94180c8d
kush925 <kushagra8d@gmail.com>
2025-06-23 06:13:07 +0530

    Fix login route and structure

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,7 +1,10 @@
 'use client';
-
-import { SessionProvider } from 'next-auth/react';
+import NextAuthProvider from './SessionProvider'; // make sure path is correct
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <NextAuthProvider>
+      {children}
+    </NextAuthProvider>
+  );
 }
